25c4831663400b8536f12b9d16695991ba3b3e59
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-31 17:29:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -71,8 +71,9 @@ def adding_matz
  		{
         :yukihiro_matsumoto => {
           :known_for => "Ruby",
-          :languages => ["LISP", "C"]},
-        :grace_hopper => {
+          :languages => ["LISP", "C"]
+        },
+          :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
